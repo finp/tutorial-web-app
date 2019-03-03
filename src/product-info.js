70715584efbba54434e56e8cc8032bd76505eb60
Paul Wright <pwright@redhat.com>
2019-03-03 18:02:43 +0000

    add products to list

--- src/product-info.js
+++ src/product-info.js
@@ -1,29 +1,44 @@
 // This file needs to live inside src to be importable
 export default {
-// tag::amq-online[]
   'amq-online-standard': {
+    // tag::amq-online[]
     prettyName: 'Red Hat AMQ Online',
     gaStatus: 'preview'
-// end::amq-online[]
+    // end::amq-online[]
   },
   fuse: {
+    // tag::fuse-online[]
     prettyName: 'Red Hat Fuse Online',
     gaStatus: 'GA'
+    // end::fuse-online[]
+
   },
   codeready: {
+    // tag::codeready[]
     prettyName: 'CodeReady',
     gaStatus: 'GA'
+    // end::codeready[]
+
   },
   launcher: {
+    // tag::launcher[]
     prettyName: 'Red Hat Developer Launcher',
     gaStatus: 'community'
+    // end::launcher[]
+
   },
   '3scale': {
+    // tag::three-scale[]
     prettyName: 'Red Hat 3scale API Management Platform',
     gaStatus: 'GA'
+    // end::three-scale[]
+
   },
   apicurio: {
+    // tag::apicurito[]
     prettyName: 'Apicurito',
     gaStatus: 'GA'
+    // end::apicurito[]
+
   }
 };
